82930c2ae4f3f458b4bf84727e4b09cf0e1cb1c5
Bruno Alla <bruno.alla@founders4schools.org.uk>
2016-12-21 12:43:16 +0000

    Fix Flake8 errors

--- surveys/urls.py
+++ surveys/urls.py
@@ -2,8 +2,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from . import views
 
 urlpatterns = [
     url(r'', TemplateView.as_view(template_name="base.html")),
-    ]
+]
--- tests/test_models.py
+++ tests/test_models.py
@@ -10,8 +10,6 @@ Tests for `django-surveys` models module.
 
 from django.test import TestCase
 
-from surveys import models
-
 
 class TestSurveys(TestCase):
 
