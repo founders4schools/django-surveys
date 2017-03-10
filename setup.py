23b2c911d7622a7f910a395f7d2de3ac4a657fd9
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-03-10 17:38:46 +0000

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -9,36 +9,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
-def get_version(*file_paths):
-    """Retrieves the version from surveys/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-
-version = get_version("surveys", "__init__.py")
-
+with open('surveys/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
 if sys.argv[-1] == 'publish':
     try:
         import wheel
+
         print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
     os.system('python setup.py sdist upload')
     os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
     sys.exit()
 
 readme = open('README.rst').read()
