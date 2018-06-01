09c42774d381dc98e87a190c729b5200d3d694f2
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-06-01 17:37:43 +0100

    Remove Django 2.0 from allow_failures, update changelog & classifiers

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
