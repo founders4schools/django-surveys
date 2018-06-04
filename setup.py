95872b4f1cd4163ee44e20d6ef3bc335a2452a9f
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-06-04 10:11:38 +0100

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django-surveys',
-    version='1.0.0',
+    version='1.1.0',
     description="""A reusable Django app that lets users write feedback for any model""",
     long_description=readme + '\n\n' + history,
     author='Founders4Schools',
